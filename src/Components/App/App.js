fceed5c50d0f881512d53a755a784e8b0128ae69
uKioops <uKioops@users.noreply.github.com>
2022-08-12 14:03:02 +0200

    62

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -37,6 +38,16 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name) {
+
+    this.setState({playlistName: name});
+
+  }
+
+  savePLaylist() {
+    
+  }
+
   render() {
 
   
@@ -47,7 +58,8 @@ class App extends React.Component {
      <SearchBar />
       <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}  />
-      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}
+      onNameChange={this.updatePlaylistName}/>
       </div>
     </div>
     </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,11 +3,22 @@ import { TrackList } from '../TrackList/TrackList'
 import './Playlist.css'
 
 export class Playlist extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this)
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
+
     render() {
 
     return(
         <div className="Playlist">
-        <input defaultValue={this.props.playlistName}/>
+        <input defaultValue={this.props.playlistName} onChange={this.handleNameChange}/>
         <br/>
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
